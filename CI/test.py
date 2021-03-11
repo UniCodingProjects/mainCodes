604b29c3774908019874a3ed6c43cc151caa3c48
OpenUniProjects <cryos10@gmail.com>
2021-03-11 18:54:26 +0200

    change travis os to lnx

--- CI/test.py
+++ CI/test.py
@@ -1,10 +1,15 @@
 import os
+import pytest
 
-ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "..\\"))
-BASE_FOLDERS = ["Alexandrax", "Roman", "Pavel", ".git", ".idea", "CI"]
 
-folders = os.scandir(ROOT_DIR)
-for folder in folders:
-    if os.path.isdir(folder.path):
-        print(f"testing: {folder.name}")
-        assert folder.name in BASE_FOLDERS
+@pytest.mark.CI
+def test_CI():
+    ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "..\\"))
+    BASE_FOLDERS = ["Alexandrax", "Roman", "Pavel", ".git", ".idea", "CI"]
+
+    folders = os.scandir(ROOT_DIR)
+    for folder in folders:
+        if os.path.isdir(folder.path):
+            if folder.name not in ["__pycache__", '.pytest_cache']:
+                print(f"testing: {folder.name}")
+                assert folder.name in BASE_FOLDERS
