06aaf6b311d511a3d65e1ae7d937bd3f1ff40a14
Pavel-codes <fabelbutov79@gmail.com>
2021-04-13 20:21:39 +0200

    Maman 12 - finishing touch

--- Pavel/Tasks/Point.java
+++ Pavel/Tasks/Point.java
@@ -1,85 +1,150 @@
+/** Point - a class that presents a point in a 2 dimensional plane using (x,y) coordinates
+ *
+ *  @Author Pavel Butov
+ * @version Maman 12
+ */
 public class Point {
     private double _x;
     private double _y;
 
-    public Point(double x, double y){
+    /**
+     * Constructor
+     * obtains new x, y coordinates
+     *
+     * @param x
+     * @param y
+     */
+    public Point(double x, double y) {
         _x = x;
         _y = y;
     }
-    public Point(Point other){
+
+    /**
+     * Constructor
+     *
+     * instantiates a new object of the Point type
+     * @param other reference object for coordinate copy
+     */
+    public Point(Point other) {
         _x = other._x;
         _y = other._y;
     }
+
+    /**
+     * Get x double
+     *
+     * @return obtaining x coordinate of the Point object
+     */
     //Getters
-    public double getX(){
+    public double getX() {
         return _x;
     }
-    public double getY(){
+
+    /**
+     * Get y double
+     *
+     * @return obtaining y coordinate of the Point object
+     */
+    public double getY() {
         return _y;
     }
+
+    /**
+     * Set x double
+     *
+     * @param num sets x coordinate of the Point object
+     */
     //Setters
-    public void setX(double num){
+    public void setX(double num) {
         _x = num;
     }
-    public void setY(double num){
+    /**
+     * Set x double
+     *
+     * @param num sets y coordinate of the Point object
+     */
+    public void setY(double num) {
         _y = num;
     }
-    public String toString(){
+
+    /** toString
+     *
+     * @return returns a string representation of the point's coordinates
+     */
+    public String toString() {
         return ("(" + _x + "," + _y + ")");
     }
-    public boolean equals(Point other){
+
+    /**
+     * equals boolean
+     *
+     * @param other point object used for comparison
+     * @return returns true if coordinates of both points are equal or false if not
+     */
+    public boolean equals(Point other) {
         return ((_x == other._x) && (_y == other._y));
     }
-    public boolean isAbove (Point other){
+
+    /**
+     * isAbove boolean
+     *
+     * @param other point object used for comparison
+     * @return returns true if the point is above the compared point or false if not
+     */
+    public boolean isAbove(Point other) {
         return ((_y > other._y));
     }
-    public boolean isUnder (Point other){
+
+    /**
+     * isUnder boolean
+     *
+     * @param other point object used for comparison
+     * @return returns true if the point is under the compared point or false if not
+     */
+    public boolean isUnder(Point other) {
         return other.isAbove(this);
     }
-    public boolean isLeft(Point other){
+
+    /**
+     * isLeft boolean
+     *
+     * @param other point object used for comparison
+     * @return returns true if the point is located left of the compared point or false if not
+     */
+    public boolean isLeft(Point other) {
         return ((_x < other._x));
     }
-    public boolean isRight(Point other){
+
+    /**
+     * isRight boolean
+     *
+     * @param other point object used for comparison
+     * @return returns true if the point is located right of the compared point or false if not
+     */
+    public boolean isRight(Point other) {
         return other.isLeft(this);
     }
-    public double distance (Point p){
+
+    /**
+     * distance
+     *
+     * @param p point object used for comparison
+     * @return returns the distance between the two points compared
+     */
+    public double distance(Point p) {
         return Math.sqrt(Math.pow(_x - p.getX(), 2) + Math.pow(_y - p.getY(), 2));
     }
-    public int quadrant(){
-        if(_x > 0 && _y > 0){
-            return 1;
-        }
-        if(_x < 0 && _y > 0){
-            return 2;
-        }
-        if(_x < 0 && _y < 0){
-            return 3;
-        }
-        if(_x > 0 && _y < 0){
-            return 4;
-        }else{
-            if(_x == 0 || _y == 0);
-            return 0;
-        }
-    }
 
-    public static void main(String[] args){
-        Point myPoint = new Point(2, 2);
-        System.out.println("x value of myPoint " + myPoint.getX());
-        Point myPoint2 = new Point(myPoint);
-        System.out.println("x value of myPoint2 " + myPoint2.getX());
-        Point myPoint3 = new Point(myPoint);
-        myPoint3.setX(3);
-        myPoint3.setY(5);
-        myPoint.setX(0);
-//        System.out.println("myPoint x value after change  " + myPoint.getX());
-        System.out.println("get values as string " + myPoint.toString());
-        System.out.println("is equal " + myPoint.equals(myPoint2));
-        System.out.println("is above " + myPoint.isAbove(myPoint2));
-        System.out.println("is under " + myPoint.isUnder(myPoint2));
-        System.out.println("is left " + myPoint.isLeft(myPoint2));
-        System.out.println("is right " + myPoint.isRight(myPoint2));
-        System.out.println("The distance is " + myPoint.distance(myPoint2));
-        System.out.println("Quadrant is " + myPoint2.quadrant());
+    /**
+     * quadrant
+     *
+     * @return returns the number of quadrant the point is located at
+     */
+    public int quadrant() {
+        if (_x > 0 && _y > 0) return 1;
+        if (_x < 0 && _y > 0) return 2;
+        if (_x < 0 && _y < 0) return 3;
+        if (_x > 0 && _y < 0) return 4;
+        return 0;
     }
 }
