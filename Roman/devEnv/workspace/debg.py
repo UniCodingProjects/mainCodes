a7e5ae8947ab0f2bc931cff17b4129564c16f2ee
OpenUniProjects <cryos10@gmail.com>
2021-08-09 23:49:56 +0300

    update roman fold

--- Roman/devEnv/workspace/debg.py
+++ Roman/devEnv/workspace/debg.py
@@ -65,46 +65,95 @@ def deepCopy(head: Node):
     return newL
 
 
-n1 = Node(1)
-n2 = Node(2)
-n3 = Node(3)
-n4 = Node(4)
-n5 = Node(5)
+def assertDeepCopy():
+    n1 = Node(1)
+    n2 = Node(2)
+    n3 = Node(3)
+    n4 = Node(4)
+    n5 = Node(5)
 
-n1.setNext(n2)
-n2.setNext(n3)
-n3.setNext(n4)
-n4.setNext(n5)
+    n1.setNext(n2)
+    n2.setNext(n3)
+    n3.setNext(n4)
+    n4.setNext(n5)
 
-n1.setRand(n3)
-n3.setRand(n5)
-n5.setRand(n2)
+    n1.setRand(n3)
+    n3.setRand(n5)
+    n5.setRand(n2)
+
+    l1 = LinkedList(n1)
+    l2 = deepCopy(n1)
+
+    l1.toPrint()
+    l2.toPrint()
+
+    assert l1 != l2
+    temp = l1.getHead()
+    temp2 = l2.getHead()
+    while temp is not None:
+        print(f"l1: {temp.getData()}", end=", ")
+        print(f"l2: {temp2.getData()}")
+        assert temp != temp2
+        temp = temp.getNext()
+        temp2 = temp2.getNext()
+
+    temp = l1.getHead()
+    temp2 = l2.getHead()
+    while temp is not None:
+        if temp.getRand() is not None:
+            print(f"l1: {temp.getRand().getData()}")
+            print(f"l2: {temp2.getRand().getData()}")
+            assert temp.getRand() != temp2.getRand()
+        temp = temp.getNext()
+        temp2 = temp2.getNext()
 
 
-l1 = LinkedList(n1)
-l2 = deepCopy(n1)
+class Solution:
+    def reverseWithString(self, x: int) -> int:
+        y = abs(x)
+        charArr = list(str(y))
+        i, j = 0, len(charArr) - 1
+        while i < j:
+            charArr[i], charArr[j] = charArr[j], charArr[i]
+            i += 1
+            j -= 1
+        if abs(int("".join(charArr))).bit_length() >= 32:
+            return 0
+        if x < 0:
+            return int("".join(charArr)) * -1
+        return int("".join(charArr))
 
-l1.toPrint()
-l2.toPrint()
+    @staticmethod
+    def reverse(x: int) -> int:
+        def findLen(y):
+            counter = 1
+            while y >= 1:
+                y = y / 10
+                counter *= 10
+            return counter
 
-assert l1 != l2
-temp = l1.getHead()
-temp2 = l2.getHead()
-while temp is not None:
-    print(f"l1: {temp.getData()}", end=", ")
-    print(f"l2: {temp2.getData()}")
-    assert temp != temp2
-    temp = temp.getNext()
-    temp2 = temp2.getNext()
+        def checkBit(newX, x):
+            lowLimit = pow(-2, 31)
+            highLimit = pow(2, 31) - 1
+            if x > 0 and newX > highLimit:
+                return 0
+            elif x < 0 and (newX * - 1) < lowLimit:
+                return 0
 
-temp = l1.getHead()
-temp2 = l2.getHead()
-while temp is not None:
-    if temp.getRand() is not None:
-        print(f"l1: {temp.getRand().getData()}")
-        print(f"l2: {temp2.getRand().getData()}")
-        assert temp.getRand() != temp2.getRand()
-    temp = temp.getNext()
-    temp2 = temp2.getNext()
+        y = abs(x)
+        newX = 0
+        length = findLen(y)
+        while length != 1:
+            newX += y.__mod__(10) * length
+            y = y // 10
+            length //= 10
+        newX = newX // 10
+        if checkBit(newX, x) == 0:
+            return 0
+        if x < 0:
+            return newX * -1
+        return newX
 
-pass
+
+s = Solution()
+print(s.reverse(-2147483412))
