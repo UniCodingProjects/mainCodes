119ef1edd30f571f3aebd924ebe6ee7ff5ef8113
Sasha-Lisits <lisits.alexandra@yandex.by>
2021-10-09 19:40:17 +0300

    adding ID exersice

---
+++ Alexandrax/ID.c
@@ -0,0 +1,31 @@
+//#include <stdio.h>
+//
+//void  printDigit(int N){
+//
+//    int r,j;
+//    int arr[9] = {};
+//    int i = 0;
+//
+//    while (N !=0){
+//        r = N % 10;
+//        arr[i] = r;
+//        i++;
+//        N = N / 10;
+//    }
+//    for( j = i-1;j > -1; j--){
+//        printf("The digit num  is[%d] =  %d\n", j,arr[j]);
+//    }
+//}
+//int main()
+//{
+//    int N;
+//     printf("Please enter you ID number: ");
+//     scanf("%d",&N);
+//     if (N /10 >=1000000000 || N <= 99999999){
+//         printf("ERROR!!!!!!! ");
+//     }
+//     else{
+//         printDigit(N);
+//     }
+//    return 0;
+//}
