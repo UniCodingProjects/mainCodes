8482beb274ca02e733f3ad44fb318a4b3258a945
OpenUniProjects <cryos10@gmail.com>
2021-10-22 14:16:21 +0300

    roman: rename C folder

--- Roman/C/maman11/contract/contract.c
+++ Roman/C/maman11/contract/contract.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 #include <ctype.h>
 
-int toPrintArr(char s[]){
+void toPrintArr(char s[]){
     int i;
     for (i=0;i<81;i++){
-        if (s[i] == '\000') break;
+        if (s[i] == '\000' || (int) s[11] == 39) break;
         printf("%c", s[i]);
     }
     printf("\n");
-    return 0;
 }
 
-void setSequenceStop(char s2[], char first, char last, int idx){
+void setSequenceStop(char s2[], char first, char last, int idx, int *outPutIdx, int *lastIdx, int *i, char *sequential){
     s2[idx] = first;
     s2[idx + 1] = '-';
     s2[idx + 2] = last;
+    *outPutIdx += 3;
+    *lastIdx = *i+1;
+    *sequential = 0;
 }
 
 void setNonSeqStop(char s2[], char curr, int *outPutIdx, int *lastIdx, int *i, char *sequential){
@@ -34,16 +36,20 @@ int getInputLen(char s1[]){
     return counter;
 }
 
-int fillOutput(char s1[], char s2[], int len){
+void fillOutput(char s1[], char s2[], int len){
     int i = 0, lastIdx = 0, outPutIdx = 0;
     char curr, next, isSpace = 0, sequential = 0;
 
     for (i=0; i<len; i++) {
         curr = s1[i];
         next = s1[i+1];
-        if (curr == '\n') break;
+        if (curr == '\n' || curr == EOF) break;
         if (curr == ' ') isSpace = 1; else isSpace = 0;
         if (!isalnum(curr) || curr == next){
+            if (sequential > 1 && isSpace == 0){
+                setSequenceStop(s2, s1[lastIdx], curr, outPutIdx, &outPutIdx, &lastIdx, &i, &sequential);
+                continue;
+            }
             setNonSeqStop(s2, curr, &outPutIdx, &lastIdx, &i, &sequential);
             continue;
         }
@@ -52,10 +58,7 @@ int fillOutput(char s1[], char s2[], int len){
             continue;
         }
         if ((sequential > 1) && isSpace == 0){
-            setSequenceStop(s2, s1[lastIdx], curr, outPutIdx);
-            outPutIdx += 3;
-            lastIdx = i+1;
-            sequential = 0;
+            setSequenceStop(s2, s1[lastIdx], curr, outPutIdx, &outPutIdx, &lastIdx, &i, &sequential);
             continue;
         }
         else{
@@ -65,19 +68,13 @@ int fillOutput(char s1[], char s2[], int len){
                 continue;
             }
             setNonSeqStop(s2,  s1[lastIdx], &outPutIdx, &lastIdx, &i, &sequential);
-            continue;
         }
     }
-    return outPutIdx;
 }
 
 void contract(char s1[], char s2[]){
     int len = getInputLen(s1);
     fillOutput(s1, s2, len);
-}
-
-void clear(){
-    printf("\e[1;1H\e[2J");
 }
 
 int main(){
@@ -88,7 +85,6 @@ int main(){
     fgets(input, 81, stdin);
 
     contract(input, output);
-    clear();
     printf("Input sequence: ");
     toPrintArr(input);
     printf("Output sequence: ");
